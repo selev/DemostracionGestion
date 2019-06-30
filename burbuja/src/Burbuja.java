c79535329da24cdc72402f656b5d503f633b22b1
Raul <Raul@Raul>
2019-06-30 18:50:23 -0500

    #1 proyecto de prueba Gestion de software compartido

---
+++ burbuja/src/Burbuja.java
@@ -0,0 +1,44 @@
+import java.io.*;
+ 
+public class Burbuja
+{
+    public static void main(String arg[]) throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("\n Ingrese Numero de Datos a Ingresar : ");
+        int tam = Integer.parseInt(in.readLine());
+
+        int arr[] = new int[tam];
+        System.out.println();
+
+        int j = 0;
+        for (int i = 0 ; i < arr.length;i++)
+        {
+            j+=1;
+            System.out.print("Elemento " + j + " : ");
+            arr[i] = Integer.parseInt(in.readLine());
+        }
+        burbuja(arr);
+    }
+ 
+    static void burbuja(int arreglo[])
+    {
+        for(int i = 0; i < arreglo.length - 1; i++)
+        {
+            for(int j = 0; j < arreglo.length - 1; j++)
+            {
+                if (arreglo[j] < arreglo[j + 1])
+                {
+                    int tmp = arreglo[j+1];
+                    arreglo[j+1] = arreglo[j];
+                    arreglo[j] = tmp;
+                }
+            }
+        }
+        for(int i = 0;i < arreglo.length; i++)
+        {
+            System.out.print(arreglo[i]+"\n");
+        }
+    }
+}
